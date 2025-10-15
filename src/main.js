f190a0f1bbacd7ca95b3364b436884a83e5060a5
Zaryab Ahmad <zaryabahmad454@gmail.com>
2025-10-15 22:20:18 +0500

    Setup coming soon

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import './style.css'
+
+const app = document.querySelector('#app')
+
+app.innerHTML = `
+  <section class="wrapper" aria-labelledby="title">
+    <h1 id="title" class="title">Coming Soon</h1>
+    <p class="subtitle">We are working hard to get you your photos :)</p>
+  </section>
+`
