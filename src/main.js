260125a170d1eea61c1664586ed90e6d7ee1fd6b
Zaryab Ahmad <zaryabahmad454@gmail.com>
2025-10-15 22:45:10 +0500

    Add logo and change bg color

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ const app = document.querySelector('#app')
 
 app.innerHTML = `
   <section class="wrapper" aria-labelledby="title">
+    <img src="/logo-white.png" class="brand-logo" alt="GetYourPhoto logo" />
     <h1 id="title" class="title">Coming Soon</h1>
     <p class="subtitle">We are working hard to get you your photos :)</p>
   </section>
